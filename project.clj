3f760b4ed7d3a5fb46b339b267b6c582c375eb53
Dennis Lipovsky <dennis.lipovsky@gmail.com>
2014-07-25 14:10:55 -0400

    implementing all the day-based public api methods

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-oauth "1.5.1"]
-                 [clj-http "0.9.2"]])
+                 [clj-http "0.9.2"]
+                 [org.clojure/data.json "0.2.5"]])
--- src/fitbit_public/client.clj
+++ src/fitbit_public/client.clj
@@ -1,7 +1,8 @@
 (ns fitbit-public.client
 	(:require [oauth.client :as oauth]
             [clojure.edn :as edn]
-            [clj-http.client :as http]))
+            [clj-http.client :as http]
+            [clojure.data.json :as json]))
 
 (def settings
   (with-open [in (java.io.PushbackReader. (clojure.java.io/reader "settings.edn"))]
@@ -12,9 +13,11 @@
    :access-token  "https://api.fitbit.com/oauth/access_token"
    :authorize     "https://www.fitbit.com/oauth/authorize"})
 
-(defn profile [user]
-    (let [url (str "http://api.fitbit.com/1/user/" user "/activities/date/2014-07-24.json")
-        consumer (oauth/make-consumer 
+(defn oauth-headers 
+  ([url] 
+    (oauth-headers url false))
+  ([url debug]
+    (let [consumer (oauth/make-consumer 
                    (:consumer-key settings) 
                    (:consumer-secret settings)
                    (:request-token endpoints)
@@ -24,6 +27,59 @@
         request-token (oauth/request-token consumer)
         credentials (oauth/credentials consumer
                       nil nil
-                      :POST url {})
-        headers (oauth/build-request (dissoc credentials :oauth_token))]
-    (http/post url (merge headers))))
+                      :GET url {})]
+    (oauth/build-request (dissoc credentials :oauth_token)))))
+
+(defn make-url [user-id]
+  (str "http://api.fitbit.com/1/user/" user-id))
+
+(defn api-request [url headers debug]
+  (json/read-str (:body (http/get url (merge headers {:throw-exceptions false} (if debug {:debug true}))))))
+
+(defn profile 
+  ([user-id] 
+    (profile false))
+  ([user-id debug]
+    (let [url (str (make-url user-id) "/profile.json")
+          headers (oauth-headers url)]
+      (api-request url headers debug))))
+
+(defn activities 
+  ([user-id date]
+    (activities user-id date false))
+  ([user-id date debug] 
+    (let [url (str (make-url user-id) "/activities/date/" date ".json")
+          headers (oauth-headers url)]
+      (api-request url headers debug))))
+
+(defn body
+  ([user-id date]
+    (body user-id date false))
+  ([user-id date debug]
+    (let [url (str (make-url user-id) "/body/date/" date ".json")
+          headers (oauth-headers url)]
+      (api-request url headers debug))))
+
+(defn food-log
+  ([user-id date]
+    (food-log user-id date false))
+  ([user-id date debug]
+    (let [url (str (make-url user-id) "/foods/log/date/" date ".json")
+          headers (oauth-headers url)]
+      (api-request url headers debug))))
+
+(defn sleep
+  ([user-id date]
+    (sleep user-id date false))
+  ([user-id date debug]
+    (let [url (str (make-url user-id) "/sleep/date/" date ".json")
+          headers (oauth-headers url)]
+      (api-request url headers debug))))
+
+(defn steps-timeseries 
+  ([user-id start-date end-date] 
+    (steps-timeseries user-id start-date end-date false))
+  ([user-id start-date end-date debug]
+    (let [url (str (make-url user-id) "/activities/steps/date/" start-date "/" end-date ".json")
+          headers (oauth-headers url)]
+      (api-request url headers debug))))
